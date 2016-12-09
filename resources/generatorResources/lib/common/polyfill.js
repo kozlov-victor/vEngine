086f8c96c58805f7d42074bb7ee43662ed7957e9
VAKozlov <kozlov-victor@rambler.ru>
2016-12-09 17:30:10 +0200

    commonJS module refactoring

--- resources/generatorResources/lib/common/polyfill.js
+++ resources/generatorResources/lib/common/polyfill.js
@@ -1,3 +1,6 @@
 Array.prototype.remove = function(el){
     this.splice(this.indexOf(el),1);
 };
+
+window.URL = window.URL || window.webkitURL;
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
